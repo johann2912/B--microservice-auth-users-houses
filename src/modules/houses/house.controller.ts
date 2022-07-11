e4725bc00745726cf6a3c386a80189c0f7e24ca4
Johann Gomez C <johanngomez02@email.com>
2022-07-11 00:24:42 -0500

    :hammer: [CREATE] CRUD houses

--- src/modules/houses/house.controller.ts
+++ src/modules/houses/house.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, Session, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post, Session, UseGuards } from "@nestjs/common";
 import { ApiBearerAuth, ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { plainToClass } from "class-transformer";
 import { AccessGuard } from "src/lib/guards/access.guard";
 import { IAccess } from "src/lib/jwt/interfaces/access";
 import { HouseCreateDtoEntry } from "./core/dto/entry/create-house.dto";
 import { HouseCreateDtoResponse } from "./core/dto/response/create-house.response";
+import { HouseDeleteDtoResponse } from "./core/dto/response/delete-house.response";
 import { HouseService } from "./house.service";
 
 
@@ -13,8 +14,35 @@ import { HouseService } from "./house.service";
 export class HouseController {
     constructor(private readonly houseService: HouseService){};
 
+    @Get('all')
+    @ApiOperation({ summary: `houses to be listed by users, 
+    can only be created with an ADMIN user role` })
+    @UseGuards(AccessGuard)
+    @ApiBearerAuth()
+    @ApiOkResponse({type: [HouseCreateDtoResponse] })
+    async allHouses(
+        @Session() payload: IAccess
+    ){
+        const houses = await this.houseService.all(payload.id);
+        return plainToClass(HouseCreateDtoResponse, houses, {excludeExtraneousValues:true});
+    };
+
+    @Get('by-id/:houseId')
+    @ApiOperation({ summary: `houses to be listed by users, 
+    can only be created with an ADMIN user role` })
+    @UseGuards(AccessGuard)
+    @ApiBearerAuth()
+    @ApiOkResponse({type: HouseCreateDtoResponse })
+    async searchHouse(
+        @Session() payload: IAccess,
+        @Param('houseId', ParseUUIDPipe) houseId:string
+    ){
+        const house = await this.houseService.searchHouse(payload.id, houseId);
+        return plainToClass(HouseCreateDtoResponse, house, {excludeExtraneousValues:true});
+    };
+
     @Post('create')
-    @ApiOperation({ summary: `houses to be reserved by clients, 
+    @ApiOperation({ summary: `houses to be reserved by users, 
     can only be created with an ADMIN user role` })
     @UseGuards(AccessGuard)
     @ApiBearerAuth()
@@ -27,4 +55,16 @@ export class HouseController {
         return plainToClass(HouseCreateDtoResponse, house, {excludeExtraneousValues:true});
     };
 
-}
+    @Delete('delete/:houseId')
+    @ApiOperation({ summary: `houses can only be deleted by an ADMIN role user` })
+    @UseGuards(AccessGuard)
+    @ApiBearerAuth()
+    @ApiOkResponse({type: HouseDeleteDtoResponse })
+    async deleteHouse(
+        @Param('houseId', ParseUUIDPipe) houseId: string,
+        @Session() payload: IAccess,
+    ){
+        const house = await this.houseService.deleteHouse(payload.id, houseId);
+        return plainToClass(HouseDeleteDtoResponse, house, {excludeExtraneousValues:true});
+    };
+};
--- src/modules/houses/house.service.ts
+++ src/modules/houses/house.service.ts
@@ -11,16 +11,50 @@ export class HouseService {
         private exceptions: ExceptionsService,
     ) {};
 
+    async all(userId){
+        await this.verifyRoleUser(userId);
+        const houses = await this.databaseService.houses.findAll();
+        if(!houses.length || houses.length < 0) this.exceptions.notFoundException({
+            message: 'houses does not found'
+        });
+
+        return houses;
+    };
+
+    async searchHouse(userId, houseId){
+        await this.verifyRoleUser(userId);
+        const house = await this.searchHouseById(houseId);
+
+        return house;
+    }
+
     async createHouse(userId, houseData: ICreateHouse){
         await this.verifyRoleUser(userId);
         const house = await this.databaseService.houses.create(houseData);
         return house;
-    }
+    };
+
+    async deleteHouse(userId, houseId){
+        await this.verifyRoleUser(userId);
+        const house = await this.searchHouseById(houseId);
+        await this.databaseService.houses.delete(house.id);
+        
+        return  { message: 'house successfully deleted' };
+    };
 
     private async verifyRoleUser(userId){
         const user = await this.databaseService.users.findOne(userId);
         if(user.role !== Roles.ADMIN) this.exceptions.badRequestException({
             message: 'the user does not have permissions'
         });
+    };
+
+    private async searchHouseById(houseId){
+        const house = await this.databaseService.houses.findOne(houseId);
+        if(!house) this.exceptions.notFoundException({
+            message: 'house does not found'
+        });
+
+        return house;
     }
 };
